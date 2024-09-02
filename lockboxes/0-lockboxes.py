c57c1ff03b76ff55df8ed461dcd5e1ba9d836084
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-02 08:59:31 +0200

    lockboxes push

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+
+
+def canUnlockAll(boxes):
+    new_keys = boxes[0]
+    temp_new_keys = []
+    openned_boxes = [0]
+    
+    while True:
+        for i in new_keys:
+            if i not in openned_boxes:
+                openned_boxes.append(i)
+                for j in boxes[i]:
+                    if j not in openned_boxes:
+                        temp_new_keys.append(j)
+        if temp_new_keys == []:
+            break
+        new_keys = temp_new_keys
+        temp_new_keys = []
+
+    return len(openned_boxes) == len(boxes);
