33cec24fa9645e7eacaec7738d367390d7f31a0b
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-03 15:03:21 +0200

    last changes on insert_in_sorted_linked_list

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	if ((*head) == NULL)
+	{
+		head = &new;
+		return (new);
+	}
+
+	while ((*head)->next != NULL && (*head)->next->n < number)
+		head = &(*head)->next;
+
+	new->next = (*head)->next;
+	(*head)->next = new;
+
+	return (new);
+}
--- insert_in_sorted_linked_list/linked_lists.c
+++ insert_in_sorted_linked_list/linked_lists.c
@@ -72,26 +72,3 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
-
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	new->next = NULL;
-
-	if (head == NULL)
-	{
-		head = new;
-		return (new);
-	}
-
-	while (head->next != NULL && head->next->n < number)
-		head = head->next;
-
-	new->next = head->next;
-	head->next = new;
-
-	return (new);
-}
