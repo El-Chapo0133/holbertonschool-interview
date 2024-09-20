ea8faecf51baf0c4dcb70e23b4469333a4339a8d
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-20 11:20:55 +0200

    Pushed last changes

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    factors = []
+
+    while n % 2 == 0:
+        factors.append(2)
+        n = n // 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n = n // i
+
+    return sum(factors)
