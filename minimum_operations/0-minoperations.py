8fe2d017b3cfa897f447a2147f341fb892863fc9
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-20 11:28:39 +0200

    Pushed last changes

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -10,13 +10,11 @@ import math
 def minOperations(n):
     factors = []
 
-    while n % 2 == 0:
-        factors.append(2)
-        n = n // 2
+    prime_factors = lambda n: [i for i in range(2, n+1) if n%i == 0 and all(i % j != 0 for j in range(2, int(i**0.5)+1))]
 
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            factors.append(i)
-            n = n // i
+    while n > 1:
+        for factor in prime_factors(n):
+            factors.append(factor)
+            n = n // factor
 
     return sum(factors)
