7e0c808a9cc4d1630612f8004538cf91ccda135a
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-11 15:00:24 +0200

    Pushed last changes

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -10,17 +10,16 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	/* int is_stable; */
+	int is_stable;
 
 	add_grids(grid1, grid2);
-	/*
 	is_stable = is_grid_stable(grid1);
 	while (!is_stable)
 	{
+		print_grid(grid1);
 		next_step(grid1);
 		is_stable = is_grid_stable(grid1);
 	}
-	*/
 }
 
 /**
