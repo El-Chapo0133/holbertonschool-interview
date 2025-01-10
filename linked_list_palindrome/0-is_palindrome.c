90eec93bd8cea741125dca7424a2891de9e8636f
El-Chapo0133 <levequeloris66@gmail.com>
2025-01-10 13:55:48 +0100

    Pushed last changes

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,62 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  0-is_palindrome.c
+ *
+ *    Description:  check if a given linked list is palindrome
+ *
+ *        Version:  1.0
+ *        Created:  10.01.2025 13:35:38
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  
+ *
+ * ==========================================================================
+ */
+#include <stdio.h>
+#include "lists.h"
+
+
+
+listint_t *reverse_list(listint_t *head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (prev);
+}
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *second_half = *head;
+
+	if (!head || !(*head)->next)
+		return (0);
+
+	while (!temp && !temp->next)
+	{
+		second_half = second_half->next;
+		temp = temp->next->next;
+	}
+	second_half = reverse_list(second_half);
+
+	while (second_half)
+	{
+		if ((*head)->n != second_half->n)
+			return (0);
+		second_half = second_half->next;
+		(*head) = (*head)->next;
+	}
+	return (1);
+}
