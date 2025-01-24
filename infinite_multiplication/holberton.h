429b6bfeaf4b0d8194d46b6cc0006fbb2219cb3c
El-Chapo0133 <levequeloris66@gmail.com>
2025-01-24 15:34:55 +0100

    Pushed last changes

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,17 @@
+#ifndef H_MUL
+#define H_MUL
+
+#define true 1
+#define false 0
+#define MAX 2500
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void multiply(char *num_1, char *num_2);
+int is_a_number(char *a_str);
+void sum_and_print(char answer[MAX1][MAX2], int len_1, int len_2);
+int str_len(char *a);
+
+#endif
