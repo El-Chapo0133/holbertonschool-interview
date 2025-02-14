9ed4038bf6bb52ccd88d61f2dd568c03370d9ced
El-Chapo0133 <levequeloris66@gmail.com>
2025-02-14 13:26:49 +0100

    Pushed last changes

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -11,8 +11,11 @@ def makeChange(coins, total):
     while remain != 0:
         while remain < coins[i]:
             i -= 1
-            if i > 0:
+            if i <= 0:
                 return -1
         acc += 1
         remain -= coins[i]
     return acc
+
+print(makeChange([1, 2, 25], 37))
+print(makeChange([1256, 54, 48, 16, 102], 1453))
