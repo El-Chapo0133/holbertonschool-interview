f73d69c42b663ff44ea36b8bb2c42341fa3e48ae
El-Chapo0133 <levequeloris66@gmail.com>
2025-02-14 13:37:58 +0100

    Pushed last changes

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -6,19 +6,20 @@ Documented yes
 def makeChange(coins, total):
     if total < 0:
         return 0
-
     acc = 0
     i = len(coins) - 1
     remain = total
 
     if i < 0:
         return -1
+    print ("startup coins: " + str(coins) + " startup total: " + str(total))
     while remain != 0:
         while remain < coins[i]:
+            print ("coin too big: " + str(coins[i]) + "moving i: " + str(i))
             i -= 1
             if i < 0:
                 return -1
         acc += 1
         remain -= coins[i]
+        print ("remain at end of loop: " + str(remain))
     return acc
-
